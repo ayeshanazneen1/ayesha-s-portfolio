a1ef47413534551977502a9766c298776a840d05
ayeshanazneen <134217413+ayeshanazneen1@users.noreply.github.com>
2025-05-31 23:04:35 +0500

    Color scheme changed

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,9 +5,9 @@ export default function Header() {
   const sections = ["Home", "Portfolio", "About", "Testimonial", "Contact"];
 
   return (
-    <header className="bg-[#EEE9DB] w-full z-50">
+    <header className="bg-[#3F3B37] w-full z-50">
       <nav className="container mx-auto flex justify-between items-center px-8 py-6">
-        <Link href="/" className="text-2xl font-bold text-[#3F3B37]">
+        <Link href="/" className="text-2xl font-bold text-[#EEE9DB]">
           AYESHA NAZNEEN
         </Link>
         <ul className="flex space-x-8">
@@ -15,7 +15,7 @@ export default function Header() {
             <li key={sec} className="group relative">
               <a
                 href={`#${sec.toLowerCase()}`}
-                className="text-[#3F3B37] uppercase tracking-wider transition-all duration-300 hover:text-[#2C2926] hover:tracking-widest hover:scale-105"
+                className="text-[#EEE9DB] uppercase tracking-wider transition-all duration-300 hover:text-[#2C2926] hover:tracking-widest hover:scale-105"
               >
                 {sec}
               </a>
